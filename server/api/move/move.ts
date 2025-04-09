61813db34480c0d250e7b1f0ac975ad854ab71fe
ayumu203 <ochiohita@gmail.com>
2025-04-09 23:53:29 +0900

    wip:move.tsの余計なnull型を削除

--- server/api/move/move.ts
+++ server/api/move/move.ts
@@ -3,8 +3,8 @@ import { Move } from "../../types/move.type";
 
 const prisma = new PrismaClient();
 
-export async function move_getter(move_id:number):Promise<Move|null> {
-    const data:Move | null = await prisma.move.findFirst({where: {move_id} });
+export async function move_getter(move_id:number):Promise<Move> {
+    const data:Move = await prisma.move.findFirst({where: {move_id} });
     if(!data)return data;
     return data;
 }
